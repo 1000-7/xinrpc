eef06fb403abfce96156f5745cb7e2a55fc74e98
xavior.wx <xin1996whu@icloud.com>
2019-07-28 16:58:35 +0800

    客户端固定时间去获得etcd服务注册情况

--- common/src/main/java/info/unclewang/etcd/EtcdRegister.java
+++ common/src/main/java/info/unclewang/etcd/EtcdRegister.java
@@ -135,6 +135,4 @@ public class EtcdRegister implements Register {
 		}
 		return res;
 	}
-
-
 }
--- consumer/src/main/java/info/unclewang/proxy/RpcInvocationHandler.java
+++ consumer/src/main/java/info/unclewang/proxy/RpcInvocationHandler.java
@@ -2,16 +2,19 @@ package info.unclewang.proxy;
 
 import info.unclewang.client.NettyClient;
 import info.unclewang.entity.RpcRequest;
-import info.unclewang.entity.RpcResponse;
 import info.unclewang.etcd.EtcdRegister;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author unclewang
@@ -20,10 +23,12 @@ import java.util.UUID;
 @Slf4j
 public class RpcInvocationHandler<T> implements InvocationHandler {
 	private Class<T> clz;
-	private static List<InetSocketAddress> discoverInetSocketAddress = null;
+	private List<InetSocketAddress> discoverInetSocketAddress = null;
+	private EtcdRegister register;
 
 	public RpcInvocationHandler(Class<T> clz) {
 		this.clz = clz;
+		updateDiscoverInetSocketAddress();
 	}
 
 	@Override
@@ -41,26 +46,44 @@ public class RpcInvocationHandler<T> implements InvocationHandler {
 		request.setMethodName(methodName);
 		request.setParameterTypes(parameterTypes);
 		request.setParameters(args);
-		return send(request).getResult();
-	}
-
-	private RpcResponse send(RpcRequest request) {
-
-		if (discoverInetSocketAddress == null) {
-			EtcdRegister register = new EtcdRegister();
-			register.initEtcd();
-			discoverInetSocketAddress = register.discover(this.clz.getName());
-			if (discoverInetSocketAddress.size() == 0) {
+		if (this.discoverInetSocketAddress == null) {
+			if (this.register == null) {
+				this.register = new EtcdRegister();
+				this.register.initEtcd();
+			}
+			this.discoverInetSocketAddress = this.register.discover(this.clz.getName());
+			if (this.discoverInetSocketAddress.size() == 0) {
 				log.error("No available service. Service name:{}", this.clz.getName());
 				System.exit(0);
 			}
 		}
 		//负载均衡 随机抽取实现 默认机器都能使用
-		InetSocketAddress randomAddress = discoverInetSocketAddress.get(new Random().nextInt(discoverInetSocketAddress.size()));
+		InetSocketAddress randomAddress = this.discoverInetSocketAddress.get(new Random().nextInt(this.discoverInetSocketAddress.size()));
 		log.info("server provider address:{}", randomAddress.toString());
 		NettyClient nettyClient = new NettyClient(randomAddress.getAddress().getHostAddress(), randomAddress.getPort());
 		nettyClient.connect(nettyClient.getInetSocketAddress());
-		return nettyClient.send(request);
+		return nettyClient.send(request).getResult();
+	}
+
+	public void updateDiscoverInetSocketAddress() {
+		log.info("updateDiscoverInetSocketAddress init");
+		ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(2, r -> {
+			Thread t = new Thread(r);
+			t.setName("info.unclewang.proxy.RpcInvocationHandler.updateDiscoverInetSocketAddress");
+			t.setDaemon(true);
+			return t;
+		});
+
+		scheduledExecutorService.scheduleAtFixedRate(() -> {
+			if (this.register == null) {
+				this.register = new EtcdRegister();
+				this.register.initEtcd();
+			}
+			this.discoverInetSocketAddress = this.register.discover(this.clz.getName());
+			log.info("updateDiscoverInetSocketAddress success, {}", new Date(System.currentTimeMillis()).toString());
+		}, 0, 3 * 6, TimeUnit.SECONDS);
+
 
 	}
+
 }
